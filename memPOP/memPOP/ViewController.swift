7d5f0ac6d55bbec76afb2cf7313e9b449fe1b12d
nla52 <nla52@asb9700-e02.csil.sfu.ca>
2018-10-23 11:14:21 -0700

    Initial Project for Xcode 9.4.1

---
+++ memPOP/memPOP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memPOP
+//
+//  Created by nla52 on 10/23/18.
+//  Copyright © 2018 Iota Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
