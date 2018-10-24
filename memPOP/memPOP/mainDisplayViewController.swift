bda22082eb30c8f582920dea69fd4c9c62f7d8db
eca83 <eca83@sfu.ca>
2018-10-23 21:12:44 -0700

    Added Version 1 View Controllers

---
+++ memPOP/memPOP/mainDisplayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  mainDisplayViewController.swift
+//  memPOP
+//
+//  Created by Emily on 2018-10-23.
+//  Copyright © 2018 Iota Inc. All rights reserved.
+//
+
+import UIKit
+
+class mainDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
