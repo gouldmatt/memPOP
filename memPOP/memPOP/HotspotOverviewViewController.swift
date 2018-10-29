0cca8fda62297a1e8c820e5960344ff9dd3b08ed
mattgou1d <mattgould44@gmail.com>
2018-10-29 11:49:59 -0700

    added hotspot overview

--- memPOP/memPOP/HotspotOverviewViewController.swift
+++ memPOP/memPOP/HotspotOverviewViewController.swift
@@ -5,14 +5,30 @@
 //  Created by Emily on 2018-10-23.
 //  Copyright © 2018 Iota Inc. All rights reserved.
 //
-
+import CoreData
 import UIKit
 
 class HotspotOverviewViewController: UIViewController {
-
+    
+    @IBOutlet var hotspotName: UILabel!
+    @IBOutlet var hotspotTransportation: UILabel!
+    @IBOutlet var hotspotDescription: UILabel!
+    //@IBOutlet var hotspotImage: UIImageView!
+    
+    
+    var selectedHotspot: NSManagedObject?
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
+        
+        
+        hotspotName.text = ((selectedHotspot?.value(forKey: "name")) as? String)
+        hotspotTransportation.text = ((selectedHotspot?.value(forKey: "transportation")) as? String)
+        hotspotDescription.text = ((selectedHotspot?.value(forKey: "info")) as? String)
+        
+        // get image
+        //let image = selectedHotspot?.value(forKey: "picture") as? NSData
+        //hotspotImage.image = (UIImage(data: image! as Data)!)
+        
         // Do any additional setup after loading the view.
     }
     
@@ -25,15 +41,14 @@ class HotspotOverviewViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
