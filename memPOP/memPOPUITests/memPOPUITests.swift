c5f1fdc77585fd2a3c30fd75b443ecf349ea276d
Diego Martin <diegomartin@d142-058-076-061.wireless.sfu.ca>
2018-11-05 11:17:15 -0800

    UI tests

--- memPOP/memPOPUITests/memPOPUITests.swift
+++ memPOP/memPOPUITests/memPOPUITests.swift
@@ -9,6 +9,7 @@
 import XCTest
 
 class memPOPUITests: XCTestCase {
+    
         
     override func setUp() {
         super.setUp()
@@ -35,28 +36,30 @@ class memPOPUITests: XCTestCase {
         app.navigationBars["Memories"].buttons["Back"].tap()
     }
     
+   
     func testAddHotspotWithoutPhoto() {
+        
         let app = XCUIApplication()
         app.buttons["Start"].tap()
         app.buttons["Add Hotspot"].tap()
-        app.scrollViews.children(matching: .textField).element(boundBy: 0).tap()
         
+        let scrollViewsQuery = app.scrollViews
+        scrollViewsQuery.children(matching: .textField).element(boundBy: 0).tap()
         
-        let aKey = app.keys["a"]
+        let aKey = app/*@START_MENU_TOKEN@*/.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         aKey.tap()
-        
-        let returnButton = app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
-        returnButton.tap()
+        aKey.tap()
         
         let app2 = app
         app2/*@START_MENU_TOKEN@*/.textFields["address"]/*[[".scrollViews.textFields[\"address\"]",".textFields[\"address\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
-        let bKey = app2/*@START_MENU_TOKEN@*/.keys["b"]/*[[".keyboards.keys[\"b\"]",".keys[\"b\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        bKey.tap()
-
-        returnButton.tap()
-        app/*@START_MENU_TOKEN@*/.scrollViews.containing(.staticText, identifier:"   About This Memory").element/*[[".scrollViews.containing(.button, identifier:\"Add Image\").element",".scrollViews.containing(.staticText, identifier:\"   Add Photos\").element",".scrollViews.containing(.staticText, identifier:\"   To-Do List\").element",".scrollViews.containing(.staticText, identifier:\"   Description\").element",".scrollViews.containing(.staticText, identifier:\"Transportation:\").element",".scrollViews.containing(.staticText, identifier:\"Category:\").element",".scrollViews.containing(.textField, identifier:\"address\").element",".scrollViews.containing(.staticText, identifier:\"Address:\").element",".scrollViews.containing(.staticText, identifier:\"Name:\").element",".scrollViews.containing(.staticText, identifier:\"   About This Memory\").element"],[[[-1,9],[-1,8],[-1,7],[-1,6],[-1,5],[-1,4],[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
-        XCUIApplication().scrollViews.otherElements.buttons["Done"].tap()
+        let pKey = app2/*@START_MENU_TOKEN@*/.keys["p"]/*[[".keyboards.keys[\"p\"]",".keys[\"p\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        pKey.tap()
+        pKey.tap()
+        app2/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app2/*@START_MENU_TOKEN@*/.staticTexts["   To-Do List"].swipeRight()/*[[".scrollViews.staticTexts[\"   To-Do List\"]",".swipeUp()",".swipeRight()",".staticTexts[\"   To-Do List\"]"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/
+        scrollViewsQuery.otherElements.buttons["Done"].tap()
+        
     }
     
     func testEditButton() {
@@ -84,29 +87,46 @@ class memPOPUITests: XCTestCase {
     }
     
     func testToDoList() {
+        
         let app = XCUIApplication()
         app.buttons["Start"].tap()
         app.buttons["Add Hotspot"].tap()
-        app/*@START_MENU_TOKEN@*/.scrollViews.containing(.staticText, identifier:"   About This Memory").element/*[[".scrollViews.containing(.button, identifier:\"Add Image\").element",".scrollViews.containing(.staticText, identifier:\"   Add Photos\").element",".scrollViews.containing(.staticText, identifier:\"   To-Do List\").element",".scrollViews.containing(.staticText, identifier:\"   Description\").element",".scrollViews.containing(.staticText, identifier:\"Transportation:\").element",".scrollViews.containing(.staticText, identifier:\"Category:\").element",".scrollViews.containing(.textField, identifier:\"address\").element",".scrollViews.containing(.staticText, identifier:\"Address:\").element",".scrollViews.containing(.staticText, identifier:\"Name:\").element",".scrollViews.containing(.staticText, identifier:\"   About This Memory\").element"],[[[-1,9],[-1,8],[-1,7],[-1,6],[-1,5],[-1,4],[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
         
-        let elementsQuery = app.scrollViews.otherElements
+        let scrollViewsQuery = app.scrollViews
+        scrollViewsQuery.children(matching: .textField).element(boundBy: 0).tap()
+        
+        let aKey = app/*@START_MENU_TOKEN@*/.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        aKey.tap()
+        aKey.tap()
+        
+        let app2 = app
+        app2/*@START_MENU_TOKEN@*/.textFields["address"]/*[[".scrollViews.textFields[\"address\"]",".textFields[\"address\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let bKey = app2/*@START_MENU_TOKEN@*/.keys["b"]/*[[".keyboards.keys[\"b\"]",".keys[\"b\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        bKey.tap()
+        bKey.tap()
+        
+        let returnButton = app2/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+        returnButton.tap()
+        
+        let elementsQuery = scrollViewsQuery.otherElements
         elementsQuery.textFields["Enter an item and click on '+' to add to your list"].tap()
         
-        let key = app/*@START_MENU_TOKEN@*/.keys["1"]/*[[".keyboards.keys[\"1\"]",".keys[\"1\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        key.tap()
-        key.tap()
-        
-        let button = elementsQuery.buttons["Button"]
-        button.tap()
-        
-        let key2 = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        key2.tap()
-        key2.tap()
-        button.tap()
-        app/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let tKey = app2/*@START_MENU_TOKEN@*/.keys["t"]/*[[".keyboards.keys[\"t\"]",".keys[\"t\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        tKey.tap()
+        tKey.tap()
+        returnButton.tap()
+        elementsQuery.buttons["Button"].tap()
+        app2/*@START_MENU_TOKEN@*/.staticTexts["   To-Do List"]/*[[".scrollViews.staticTexts[\"   To-Do List\"]",".staticTexts[\"   To-Do List\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp()
+        elementsQuery.buttons["Done"].tap()
+
+
     }
     
+    
     func testAddHotspotWithPhotos() {
+        
+
     
     }
     
