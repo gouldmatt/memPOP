7a4667e9454ff02f479eda18944d393d80bca602
mattgou1d <mattgould44@gmail.com>
2018-11-12 08:51:59 -0800

    Added dynamic height for the collection view and table view on the overview screen, added base for the navigation screen.

---
+++ memPOP/memPOP/navigationViewController.swift
@@ -0,0 +1,27 @@
+//
+//  navigationViewController.swift
+//  memPOP
+//
+//  Created by Matthew Gould   on 2018-11-10.
+//  Copyright © 2018 Iota Inc. All rights reserved.
+//
+
+import UIKit
+
+class navigationViewController: UIViewController {
+    
+    //===================================================================================================
+    // Functions
+    //===================================================================================================
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
