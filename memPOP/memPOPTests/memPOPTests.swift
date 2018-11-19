36bedd9ea0d2151e56da8eed81aef4a6a25e9172
mattgou1d <mattgould44@gmail.com>
2018-11-18 17:47:27 -0800

    XC test setup

--- memPOP/memPOPTests/memPOPTests.swift
+++ memPOP/memPOPTests/memPOPTests.swift
@@ -5,15 +5,16 @@
 //  Created by nla52 on 10/23/18.
 //  Copyright © 2018 Iota Inc. All rights reserved.
 //
-
+import CoreData
 import XCTest
 @testable import memPOP
+
+let fetchRequest: NSFetchRequest<HotspotMO> = HotspotMO.fetchRequest()
 
 class memPOPTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
@@ -22,6 +23,34 @@ class memPOPTests: XCTestCase {
     }
     
     func testExample() {
+        
+        
+        for i in 0 ... 9 {
+            print(i)
+            let newHotspot = HotspotMO(context: PersistenceService.context)
+            let newPhoto = PhotosMO(context: PersistenceService.context)
+            
+            newPhoto.photo = UIImageJPEGRepresentation((UIImage(named: "home"))!, 1)! as NSData
+            
+            newHotspot.name = "Home" + String(i)
+            newHotspot.address = "address" + String(i)
+            newHotspot.longitude = -122.915097
+            newHotspot.latitude = 49.277575
+            newHotspot.addToPhotos(newPhoto)
+            
+            PersistenceService.saveContext()
+        }
+ 
+        do{
+            
+            let hotspots = try PersistenceService.context.fetch(fetchRequest)
+            print(hotspots.count)
+            XCTAssert(hotspots.count == 10)
+            // Put setup code here. This method is called before the invocation of each test method in the class.
+        }
+        catch {
+            print("failed")
+        }
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
