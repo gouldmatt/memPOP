6cd4e4647a4ce349e2205cdaaf783dd9034ebed7
Matthew Gould <31941802+mattgou1d@users.noreply.github.com>
2018-12-02 18:39:09 -0800

    add missing file

---
+++ memPOP/memPOP/tutorialViewController.swift
@@ -0,0 +1,36 @@
+//
+//  tutorialViewController.swift
+//  memPOP
+//
+//  Created by Trevor Sclater on 2018-12-02.
+//  Copyright © 2018 Iota Inc. All rights reserved.
+//
+
+import UIKit
+
+class tutorialViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        // Create the alert
+//        let alert = UIAlertController(title: "Welcome to Mem-POP!", message: "Let's Set Up Your Personal Information", preferredStyle: .alert)
+//
+//        // Create the actions
+//        let startAction = UIAlertAction(title: "Get Started", style: .default) {
+//            (action:UIAlertAction) in
+//            print ("Get Started")
+//
+//            self.performSegue(withIdentifier: "personInfo", sender: self)
+//        }
+//
+//        // Add actions to alert
+//        alert.addAction(startAction)
+//
+//        // Show the alert
+//        self.present(alert,animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
